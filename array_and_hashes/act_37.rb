d1eaa46ab38279b653e8f61d72811408fabf7f74
Je Rome <jeromeabelida19@gmail.com>
2024-09-10 17:34:36 +0800

    act_37 print the name of 10th student

---
+++ array_and_hashes/act_37.rb
@@ -0,0 +1,54 @@
+students = {
+  student1: {
+    name: "Jerome",
+    age: 24,
+    grade: 88
+  },
+  student2: {
+    name: "Mark",
+    age: 20,
+    grade: 87
+  },
+  student3: {
+    name: "Juliet",
+    age: 23,
+    grade: 82
+  },
+  student4: {
+    name: "Jose",
+    age: 24,
+    grade: 81
+  },
+  student5: {
+    name: "Rose",
+    age: 21,
+    grade: 82
+  },
+  student6: {
+    name: "Yori",
+    age: 21,
+    grade: 83
+  },
+  student7: {
+    name: "Jett",
+    age: 21,
+    grade: 78
+  },
+  student8: {
+    name: "Sky",
+    age: 20,
+    grade: 78
+  },
+  student9: {
+    name: "Neon",
+    age: 24,
+    grade: 81
+  },
+  student10: {
+    name: "Chard",
+    age: 24,
+    grade: 98
+  },
+}
+
+p ("#{students[:student10][:name]}")

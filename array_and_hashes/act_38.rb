b719872bd454829507d9700e903c8ce2905c7a08
Je Rome <jeromeabelida19@gmail.com>
2024-09-10 17:52:22 +0800

    act_38 print my birthday using array

---
+++ array_and_hashes/act_38.rb
@@ -0,0 +1,19 @@
+numbers = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  ['-', 0]
+]
+my_birthday = numbers[0][1],numbers[3][1],numbers[3][1],
+  numbers[3][1],numbers[3][0],numbers[0][1], numbers[0][2],numbers[3][0],numbers[3][1],numbers[1][0]
+
+my_birthday = my_birthday.join
+
+# year = numbers[0][1],numbers[3][1],numbers[3][1],numbers[3][1]
+# year = [*year].join
+#
+# date =
+# date = [*date].join
+#
+# month = numbers[1][0],
+p (my_birthday)
